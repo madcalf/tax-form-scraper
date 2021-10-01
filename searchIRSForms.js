4e963892b1b8e405f0d0e86d555912ccff7742ac
D Dixon <d@themadcalf.com>
2021-10-01 16:40:59 -0700

    Extract product name, title, year from html response

--- searchIRSForms.js
+++ searchIRSForms.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 const formData = [];
-// const searchUrl = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?criteria=formNumber&value=Form+1040-EZ&submitSearch=Find"
 const searchUrl =
   'https://apps.irs.gov/app/picklist/list/priorFormPublication.html?criteria=formNumber&value=[FORM_NAME]&submitSearch=Find';
 
@@ -8,13 +9,35 @@ const getFormData = async (productName) => {
   console.log('getting data for...', productName);
   const req = searchUrl.replace('[FORM_NAME]', productName);
   const { data } = await axios.get(req);
-  console.log(data);
+  // need to check for error cases - e.g. no items found,
+  // parse the list of items in the html data into the required json format
+  const { document: doc } = new JSDOM(data).window;
+  const tableRows = doc.querySelectorAll('table.picklist-dataTable tr');
+  console.log(tableRows, tableRows.length);
+  if (tableRows.length > 1) {
+    tableRows.forEach((row) => {
+      // note the first row is headers and doesn't have any data we care about
+      const productName = doc
+        .querySelector('td[class=LeftCellSpacer]>a')
+        .textContent.trim();
+
+      const title = doc
+        .querySelector('td[class="MiddleCellSpacer')
+        .textContent.trim();
+
+      const year = doc
+        .querySelector('td[class="EndCellSpacer')
+        .textContent.trim();
+
+      console.log('product', productName);
+      console.log('title', title, 'year', year);
+    });
+  }
 };
 
-process.argv.forEach((arg, index) => {
-  // console.log(index, arg);
-  if (index >= 2) {
-    getFormData(arg);
-  }
+// actual args start at index 2 of argv
+const args = process.argv.slice(2);
+process.argv.forEach((arg) => {
+  getFormData(arg);
   // process.exitCode = 0;
 });
